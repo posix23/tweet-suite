ba0523ccf7af2b112e223f747b8c06470c0d5fbe
周睿哲 <zhouruizhe@zhouruizhedeMacBook-Pro.local>
2020-03-01 17:26:36 -0800

    the first list for the summary paragraph

--- scripts/summary-paragraph.R
+++ scripts/summary-paragraph.R
@@ -1,12 +1,21 @@
 library("stringr")
 library("dplyr")
-senators_df <- read.csv("data/senators.csv")
 
-# A function that takes in a dataset and returns a list of info about it:
-hashtag_vs_retweet_info <- function() {
-  senators_df %>% 
-  sen <- list() %>% 
-  sen$length <- length(senators_df) %>% 
-  return (sen)
-} 
+# senators
+get_senators_info <- function(dataset) {
+  sum <- list()
+  sum$length <- length(dataset)
+  sum$average_reply <- mean(dataset$replies,na.rm = T)
+  sum$largest_favorites <- max(dataset$favorites,na.rm = T)
+  sum$smallest_retweets <- min(dataset$retweets, na.rm = T)
+  sum$popular_party <- dataset %>% 
+    group_by(party) %>% 
+    summarise(summed_favorites = sum(favorites, na.rm = T)) %>%
+    filter(summed_favorites == max(summed_favorites, na.rm = T)) %>%
+    pull(party)
+  return (sum)
+}
+
+# Russian
+
 

0d83938aac9864cb19ee13a90c29445f553850b2
周睿哲 <zhouruizhe@zhouruizhedeMacBook-Pro.local>
2020-03-02 12:05:03 -0800

    fix linters

--- scripts/summary-table.R
+++ scripts/summary-table.R
@@ -5,29 +5,29 @@ library("knitr")
 #summary table code for the first plot: the relationship
 #between #of # and #of retweets for the senators dataset
 hashtag_vs_retweet_table <- function(dataset) {
-    dataset %>% 
+    dataset %>%
     mutate(hashtags = str_count(text, "#")) %>%
     group_by(hashtags) %>%
     summarise(retweets = sum(retweets, na.rm = T), total_tweets = n()) %>%
     mutate(retweets_per_tweet = (retweets / total_tweets)) %>%
-    select(hashtags, retweets_per_tweet) %>% 
-    kable(col.names =c("Number of Hashtags",
+    select(hashtags, retweets_per_tweet) %>%
+    kable(col.names = c("Number of Hashtags",
                        "Average Number of Retweets"), align = "l")
 }
 
 #summary table code for the regions & numbers plot
-region_numbertweets_table <- function(dataset){
-  dataset %>% 
+region_numbertweets_table <- function(dataset) {
+  dataset %>%
     filter(region != "" & region != "Unknown") %>%
     group_by(region) %>%
     summarize(number = n()) %>%
-    arrange(desc(number)) %>% 
-    select(region, number) %>% 
-    kable(col.names =c("Regions","Number of Tweets"), align = "l")
+    arrange(desc(number)) %>%
+    select(region, number) %>%
+    kable(col.names = c("Regions", "Number of Tweets"), align = "l")
 }
 
 #summary table code for the mood & date plot
-sentimental_table <- function(dataset){
+sentimental_table <- function(dataset) {
   dataset %>%
     mutate(Positive = target == 4) %>%
     count(target == 4)

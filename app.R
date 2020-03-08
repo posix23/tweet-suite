89bedbd22aa826071fe9f4df6d2cc87acffd100b
eames-palmer <jpalmer4@uw.edu>
2020-03-08 21:17:00 +0000

    Add Shiny Files

---
+++ app.R
@@ -0,0 +1,3 @@
+source("app_ui.R")
+source("app_server.R")
+shinyApp(ui = my_ui, server = my_server)
---
+++ app_ui.R
@@ -0,0 +1,8 @@
+my_ui <- navbarPage(
+  "Understanding Twitter",
+  page_one,         
+  page_two, 
+  page_three,
+  page_four,
+  page_five
+)

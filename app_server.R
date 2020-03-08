af883b29774de3fba3650fbcd2e291070c62c077
eames-palmer <jpalmer4@uw.edu>
2020-03-08 23:47:00 +0000

    Update UI and Server Info to Reflect Pages

--- app_server.R
+++ app_server.R
@@ -1,1 +1,4 @@
-my_server <- function(input, output) {}
+source("shiny_scripts/interact_one.R")
+my_server <- function(input, output) {
+  output$chart <- renderPlotly({ hashtag_vs_retweet(senators_df, input$size_range, input$party) })
+}
--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,6 @@
+source("shiny_scripts/page_one.R")
+source("Shiny_scripts/interact_one.R")
+
 my_ui <- navbarPage(
   "Understanding Twitter",
   page_one,         

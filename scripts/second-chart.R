7926bd8a97b481e52a05b24ed9e0cde28bb948d3
Toan Ly <toanly@uw.edu>
2020-03-08 22:16:55 -0700

    update for shiny

--- scripts/second-chart.R
+++ scripts/second-chart.R
@@ -11,40 +11,100 @@ convert_time <- function(datetime) {
 }
 
 # Return a new data frane that contains the mood and hour in a day
-create_mood_df <- function(dataframe) {
+create_mood_df <- function(dataframe, slider_input) {
   dataframe %>%
     mutate(time = convert_time(dataframe$date)) %>%
+    filter(time > slider_input[1], time < slider_input[2]) %>%
     select(target, time)
 }
 
 # Chart
 # This function will return the statistics for the box plot
-stats_mood <- function(dataframe, mood_number) {
-  mood_df <- create_mood_df(dataframe)
+stats_mood <- function(dataframe, mood_number, slider_input) {
+  mood_df <- create_mood_df(dataframe, slider_input)
   quantile(mood_df$time[mood_df$target == mood_number],
     probs = c(0, 0.25, 0.5, 0.75, 1)
   )
 }
 
-# This function will return a box plot
-mood <- function(dataframe) {
-  mood_df <- create_mood_df(dataframe)
+# This function will return a box plot with the user's input
+mood <- function(dataframe, drop_down_input, slider_input) {
+  mood_df <- create_mood_df(dataframe, slider_input)
+  title <- "How time affects mood"
+  x_label <- "Mood (0 for negative mood and 4 for positive mood)"
+
+  if (drop_down_input == "2") {
+    mood_df <- mood_df %>% filter(target == "4")
+    title <- "How time affects positive mood"
+    x_label <- "Positive mood"
+  } else if (drop_down_input == "3") {
+    mood_df <- mood_df %>% filter(target == "0")
+    title <- "How time affects negative mood"
+    x_label <- "Negative mood"
+  }
+
   boxplot(time ~ target, mood_df,
-    main = "How time affects mood",
-    xlab = "Mood (0 for negative mood and 4 for positive mood)",
+    main = title,
+    xlab = x_label,
     ylab = "Hour of the day", las = 1
   )
 }
 
 # Get the median for the data frame
-get_median <- function(dataframe, mood_number) {
-  mood_df <- create_mood_df(dataframe)
+get_median <- function(dataframe, mood_number, slider_input) {
+  mood_df <- create_mood_df(dataframe, slider_input)
   median(mood_df$time[mood_df$target == mood_number])
 }
 
 # Rebuild datetime from hour (12.5 -> 12:30)
 rebuild_datetime <- function(hour) {
-  min <- as.numeric(paste0("0", substr(hour, 3, nchar(hour)))) * 60
-  hour <- substr(hour, 1, 2)
-  paste0(hour, ":", round(min, digits = 0))
+  start_min <- 3
+  end_hour <- 2
+  if (hour < 10) {
+    start_min <- 2
+    end_hour <- 1
+  }
+  min <- round(as.numeric(paste0(
+    "0",
+    substr(hour, start_min, nchar(hour))
+  )) * 60,
+  digits = 0
+  )
+  hour <- substr(hour, 1, end_hour)
+  if (min < 10) {
+    min <- paste0("0", min)
+  }
+  paste0(hour, ":", min)
+}
+
+# This function will update the info paragraph based on the user's input
+info_para <- function(dataframe, drop_down_input, slider_input) {
+  para <- "The box plot for negative mood is skewed to the later time in
+    the day which could indicate that due to a long work day or school day,
+    people tend to be in a negative mood. The opposite is expected for positive
+    mood as people are just freshed out of bed."
+
+  if (drop_down_input == "2") {
+    para <- paste0(
+      "The median of the positive plot (with a range from ",
+      slider_input[1], " to ", slider_input[2], ") is at hour ",
+      get_median(dataframe, "4", slider_input), " (",
+      rebuild_datetime(get_median(
+        dataframe, "4",
+        slider_input
+      )), ")"
+    )
+  } else if (drop_down_input == "3") {
+    para <- paste0(
+      "The median of the negative plot (with a range from ",
+      slider_input[1], " to ", slider_input[2], ") is at hour ",
+      get_median(dataframe, "0", slider_input), " (",
+      rebuild_datetime(get_median(
+        dataframe, "0",
+        slider_input
+      )), ")"
+    )
+  }
+
+  return(para)
 }

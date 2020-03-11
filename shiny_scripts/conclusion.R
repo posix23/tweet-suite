2cba2d089acbb987249ffba084c3f9fe4700c8a4
Nhat Le <ledaominhnhat22@gmail.com>
2020-03-11 01:58:32 -0700

    Added Conclusion

--- shiny_scripts/conclusion.R
+++ shiny_scripts/conclusion.R
@@ -1,5 +1,5 @@
 page_five <- tabPanel(
-  "Conclusion",
+  "Conclusion", 
   fluidPage(
     h1("Conclusion"),
     br(),
@@ -31,6 +31,11 @@ page_five <- tabPanel(
       incorporate them."),
     br(),
     h2("Is There a Best Day to Tweet?"),
-    p("")
+    p("There is no particular favourite day for people to Tweet. Based on our data, we can see that the number of tweets in each day 
+      day of week is quite the same and evenly distributed. For example, the number of Tweets in Friday takes up 16% the total number of
+      tweets and the number of Tweets in Wednesday takes up 15%. Since the data is evenly distributed, we can tell that access to
+      Twitter nowdays is greatly made easier through mobile phones and other electronic devices so people can post their tweet anywhere
+      and anytime they want to with little to no difficulties.")
   )
 )
+    
